e8021cdc13a750a7dcf9d231255c1b6a5c62ad91
Jimeno0 <c.p.jimeno0@gmail.com>
2019-09-25 12:17:49 +0200

    demo completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ThemeContext } from './context'
 import {
   Background,
@@ -11,42 +11,80 @@ import {
 } from './components'
 import { Toggle } from './containers'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      scroll: 0
-    }
-    this.handleScroll = this.handleScroll.bind(this)
-  }
-
-  componentDidMount () {
-    document.addEventListener('scroll', this.handleScroll, true)
-  }
-
-  handleScroll (e) {
-    const height = e.target.scrollTop
-    this.setState({ scroll: height })
-  }
-
-  componentWillUnmount () {
-    document.removeEventListener('scroll', this.handleScroll)
-  }
-
-  render () {
-    const { scroll } = this.state
-    return (
-      <Layout>
-        <Toggle />
-        <Background scroll={scroll} />
-        <Pattern />
-        <MonogramLogo scroll={scroll} />
-        <ReactLogo scroll={scroll} />
-        <MadridLogo scroll={scroll} />
-        <Spacing id='scroll' size='200vh' />
-      </Layout>
-    )
-  }
+const App = (props) => {
+  const scroll = useScroll(0)
+  return (
+    <ThemeContext.Consumer>
+      {({ value }) => {
+        return (
+          <Layout theme={value}>
+            <Toggle />
+            <Background scroll={scroll} />
+            <Pattern />
+            <MonogramLogo scroll={scroll} />
+            <ReactLogo scroll={scroll} />
+            <MadridLogo scroll={scroll} />
+            <Spacing id='scroll' size='200vh' />
+          </Layout>
+        )
+      }}
+    </ThemeContext.Consumer>
+  )
 }
+
+const useScroll = (initialValue) => {
+  const [scroll, setScroll] = useState(initialValue)
+
+  const handleScroll = (e) => {
+    const height = e.target.scrollTop
+    setScroll(height)
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll, true)
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+    }
+  })
+  return scroll
+}
+
+// class App extends Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       scroll: 0
+//     }
+//     this.handleScroll = this.handleScroll.bind(this)
+//   }
+
+//   componentDidMount () {
+//     document.addEventListener('scroll', this.handleScroll, true)
+//   }
+
+//   handleScroll (e) {
+//     const height = e.target.scrollTop
+//     this.setState({ scroll: height })
+//   }
+
+//   componentWillUnmount () {
+//     document.removeEventListener('scroll', this.handleScroll)
+//   }
+
+//   render () {
+//     const { scroll } = this.state
+//     return (
+//       <Layout>
+//         <Toggle />
+//         <Background scroll={scroll} />
+//         <Pattern />
+//         <MonogramLogo scroll={scroll} />
+//         <ReactLogo scroll={scroll} />
+//         <MadridLogo scroll={scroll} />
+//         <Spacing id='scroll' size='200vh' />
+//       </Layout>
+//     )
+//   }
+// }
 
 export default App
--- src/containers/Toggle/index.js
+++ src/containers/Toggle/index.js
@@ -1,26 +1,48 @@
-import React, { Component, useState, useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { ToggleComponent } from '../../components'
 import { ThemeContext } from '../../context'
 
-class Toggle extends Component {
-  constructor () {
-    super()
-    this.state = { checked: false }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange (checked) {
-    this.setState({ checked })
-  }
-
-  render () {
-    return (
-      <ToggleComponent
-        onChange={this.handleChange}
-        checked={this.state.checked}
-      />
-    )
-  }
+const Toggle = (props) => {
+  const toggleHandler = useCheck(false)
+  return (
+    <ToggleComponent
+      {...toggleHandler}
+    />
+  )
 }
+
+const useCheck = (initialValue) => {
+  const [checked, onChange] = useState(initialValue)
+  const { setValue } = useContext(ThemeContext)
+
+  const handleChange = (checked) => {
+    if (!checked) setValue('dark')
+    if (checked) setValue('light')
+    onChange(checked)
+  }
+
+  return { checked, onChange: handleChange }
+}
+
+// class Toggle extends Component {
+//   constructor () {
+//     super()
+//     this.state = { checked: false }
+//     this.handleChange = this.handleChange.bind(this)
+//   }
+
+//   handleChange (checked) {
+//     this.setState({ checked })
+//   }
+
+//   render () {
+//     return (
+//       <ToggleComponent
+//         onChange={this.handleChange}
+//         checked={this.state.checked}
+//       />
+//     )
+//   }
+// }
 
 export { Toggle }
